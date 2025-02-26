a4bfe85190838e138da750b780cfd0317de2d91d
TMg00000 <magrotoni8@gmail.com>
2025-02-26 15:22:09 -0300

    Add files via upload

---
+++ SegundoDesafio/SegundoDesafio/FormatText.cs
@@ -0,0 +1,14 @@
+﻿namespace SegundoDesafio;
+
+public class FormatText
+{
+    static void Main()
+    {
+        var getNameUser = new GetName();
+        var nameUser = getNameUser.GetNameUsers();
+
+        string text = $"Olá, {nameUser.firstName.Trim()} {nameUser.lastName.Trim()}.";
+
+        Console.WriteLine(text);
+    }
+}
---
+++ SegundoDesafio/SegundoDesafio/GetName.cs
@@ -0,0 +1,17 @@
+﻿namespace SegundoDesafio;
+
+public class GetName
+{
+    public (string firstName, string lastName) GetNameUsers()
+    {
+        Console.WriteLine("Preencha as informações a baixo e aperte ENTER para confirmar.");
+  
+        var firstNameUser = Console.ReadLine();
+        Console.WriteLine("Nome:");
+
+        Console.WriteLine("Sobrenome:");
+        var lastNameUser = Console.ReadLine();
+
+        return (firstNameUser, lastNameUser);
+    }
+}
