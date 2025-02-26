b0b1e9a7740241f07d6436f85d8a3f5202eb41a4
luist <luist@JARVIS>
2025-02-26 14:51:04 -0300

    Add project files.

---
+++ PrimeiroDesafio/FormatText.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimeiroDesafio;
+
+public class FormatText
+{
+    static void Main()
+    {
+        var getNameUser = new GetName();
+        var nameUser = getNameUser.GetNameUsers();
+
+        string text = $"Olá, {nameUser.Trim()}, Seja muito bem-vindo(a)!";
+
+        Console.WriteLine(text);
+    }
+}
---
+++ PrimeiroDesafio/GetName.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeiroDesafio;
+
+public class GetName
+{
+    public string GetNameUsers()
+    {
+        Console.WriteLine("Digite seu nome abaixo: ");
+        var nameUser = Console.ReadLine();
+
+        return nameUser;
+    }
+}
