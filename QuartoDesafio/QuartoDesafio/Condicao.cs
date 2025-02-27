7b244a10ef007177f38799ef10dea0e2de496659
TMg00000 <magrotoni8@gmail.com>
2025-02-26 21:56:06 -0300

    Add files via upload

---
+++ QuartoDesafio/QuartoDesafio/Condicao.cs
@@ -0,0 +1,35 @@
+﻿namespace QuartoDesafio;
+
+public class Condicao
+{
+    public void stringVazia(string phrase)
+    {
+
+        while (phrase.Length == 0 || phrase == null)
+        {
+            Console.WriteLine("Você ainda não digitou nada.");
+            phrase = Console.ReadLine().Replace(" ", string.Empty);
+            continue;
+        }
+
+        Console.WriteLine($"A palavra ou frase digitada tem {phrase.Length} caracteres.");
+    }
+
+    public void continuar()
+    {
+        var CountWords = new CountWords();
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Deseja Continuar ? [s]im ou [n]ão");
+
+        if (Console.ReadLine().ToLower() == "s")
+        {
+            CountWords.CountWordsProMax();
+        }
+        else
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Obrigado por usar o programa.");
+        }
+    }
+}
---
+++ QuartoDesafio/QuartoDesafio/CountWords.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace QuartoDesafio;
+
+public class CountWords
+{
+    public static string CountWordsProMax()
+    {
+        var condicao = new Condicao();
+
+        Console.WriteLine("Digite uma palvra ou uma frase e descubra quantos caracteres ela tem: ");
+        var phrase = Console.ReadLine().Replace(" ", string.Empty);
+        condicao.stringVazia(phrase);
+
+        Console.WriteLine(" ");
+        condicao.continuar();
+
+        return phrase;        
+    }
+
+}
